48ef5ca702a47071585da8c378e6bd0260336b03
jatuphum <jatuphum@kkumail.com>
2021-03-09 10:21:58 +0700

    Add files via upload

---
+++ BeerApp/lib/beer.dart
@@ -0,0 +1,22 @@
+class Beer {
+  final int id;
+  final String name;
+  final String imageUrl;
+
+  Beer({this.id, this.name, this.imageUrl});
+
+  @override
+  String toString() {
+    return '\n(id=$id : name=$name)\n';
+  }
+
+  // Static method
+  // factory constructor fromJson จะแปลง json เป็น object ที่จะต้อง return instance ของ class เสมอ
+  factory Beer.fromJson(Map<String, dynamic> json) {
+    return Beer(
+      id: json['id'],
+      name: json['name'],
+      imageUrl: json['image_url'],
+    );
+  }
+}
